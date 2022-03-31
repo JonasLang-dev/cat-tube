d1469ca85ca7a1ffe50ab6bbf75b88600f93151f
cat <supercutcat@outlook.com>
2022-03-31 23:49:46 +0800

    Change video card style and add Carousel for home page

---
+++ frontend/src/components/VideoCard.tsx
@@ -0,0 +1,122 @@
+import React, { FC } from "react";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+// @ts-ignore
+import Image from "mui-image";
+import Skeleton from "@mui/material/Skeleton";
+
+interface VideoCardType {
+  path: any;
+  avatar: any;
+  title: any;
+  name: any;
+  poster: any;
+  loading?: boolean;
+}
+
+const VideoCard: FC<VideoCardType> = ({
+  path,
+  avatar,
+  poster,
+  title,
+  name,
+  loading = false,
+}) => {
+  return (
+    <Grid
+      item
+      sx={{ display: "flex", justifyContent: "center" }}
+      xs={12}
+      sm={6}
+      md={4}
+      lg={3}
+      xl={3}
+    >
+      <Card
+        sx={{
+          width: 1,
+          maxWidth: 320,
+          boxShadow:
+            "0 2px 4px -2px rgba(0,0,0,0.24), 0 4px 24px -2px rgba(0, 0, 0, 0.2)",
+        }}
+      >
+        <CardActionArea
+          to={{ pathname: "/watch", search: `?v=${path}` }}
+          component={Link}
+        >
+          <Image
+            src={
+              "https://image.freepik.com/free-photo/river-foggy-mountains-landscape_1204-511.jpg"
+            }
+            sx={{
+              width: "100%",
+              height: 0,
+              bgcolor: "rgba(0, 0, 0, 0.08)",
+              objectFit: "cover",
+            }}
+            fit="cover"
+            duration={300}
+            easing="cubic-bezier(0.7, 0, 0.6, 1)"
+            showLoading={
+              <Skeleton
+                variant="rectangular"
+                width="100%"
+                height="200px"
+                animation="wave"
+              />
+            }
+            errorIcon={true}
+            shift={null}
+            distanc="100px"
+            shiftDuration={900}
+          />
+        </CardActionArea>
+        <CardContent
+          display="grid"
+          gridTemplateAreas="'avatar title' 'avatar info'"
+          gridTemplateRows="1fr 1fr"
+          gridTemplateColumns="1fr 2fr"
+          component={Grid}
+        >
+          <Box gridArea="avatar">
+            <Avatar
+              sx={{ height: 50, width: 50 }}
+              alt="Remy Sharp"
+              src={`https://i.pravatar.cc/300?img=${avatar}`}
+            />
+          </Box>
+          <Box alignSelf="center" gridArea="title">
+            <Typography>Title</Typography>
+          </Box>
+          <Box gridArea="info">
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+              alignItems="center"
+            >
+              Remy Sharp
+            </Typography>
+            <Typography
+              textAlign="end"
+              variant="body2"
+              alignItems="center"
+              color="textSecondary"
+            >
+              {new Date().toLocaleDateString()}
+            </Typography>
+          </Box>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
+
+export default VideoCard;
--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,34 +1,12 @@
-import React, { FC, useContext, useRef } from "react";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
+import React, { FC, useState } from "react";
 import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import {
-  Avatar,
-  CardActionArea,
-  CardHeader,
-  IconButton,
-  Skeleton,
-} from "@mui/material";
-import { red } from "@mui/material/colors";
-import {
-  Download,
-  Favorite,
-  MoreVert,
-  PlayCircle,
-  Share,
-} from "@mui/icons-material";
-
-import AvatarGroup from "@mui/material/AvatarGroup";
+import VideoCard from "../../components/VideoCard";
+import Carousel from "react-material-ui-carousel";
 // @ts-ignore
 import Image from "mui-image";
-import { Link } from "react-router-dom";
+import Skeleton from "@mui/material/Skeleton";
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
@@ -42,100 +20,81 @@ const HomePage: FC<Home> = ({ loading = false, colorMode, theme }) => {
   return (
     <main>
       <Box
-        sx={
-          theme.palette.mode === "dark"
-            ? {
-                pt: 8,
-                pb: 6,
-                background:
-                  "url(src/assets/img/login-the-crown_2-1500x1000.jpg) no-repeat right #000",
-              }
-            : {
-                bgcolor: "background.paper",
-                pt: 8,
-                pb: 6,
-              }
-        }
+        sx={{
+          bgcolor: "background.paper",
+          pt: 4,
+          pb: 3,
+        }}
       >
-        <Container maxWidth="sm">
-          <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            color="text.primary"
-            gutterBottom
+        <Container maxWidth="md">
+          <Carousel
+            autoPlay={true}
+            animation="fade"
+            indicators={true}
+            duration={500}
+            navButtonsAlwaysVisible={false}
+            navButtonsAlwaysInvisible={false}
+            cycleNavigation={true}
+            fullHeightHover={true}
+            swipe={true}
+            height={400}
+            sx={{
+              borderRadius: "1rem",
+            }}
           >
-            The Chrown
-          </Typography>
-          <Typography
-            variant="h5"
-            align="center"
-            color="text.secondary"
-            paragraph
-          >
-            Adapted from historical events, the play dramatically tells the
-            story of Queen Elizabeth II, as well as the political events and
-            personal experiences of her reign.
-          </Typography>
-          <Stack
-            sx={{ pt: 4 }}
-            direction="row"
-            spacing={2}
-            justifyContent="center"
-          >
-            <Button variant="contained">Watch Now</Button>
-            <Button variant="outlined">Add to Library</Button>
-          </Stack>
+            {[
+              {
+                Name: "A",
+                Image: "https://source.unsplash.com/featured/?macbook",
+              },
+              {
+                Name: "B",
+                Image: "https://source.unsplash.com/featured/?vacuum,cleaner",
+              },
+              {
+                Name: "C",
+                Image: "https://source.unsplash.com/featured/?iphone",
+              },
+            ].map((item, index) => {
+              return (
+                <Grid item xs={4} key={item.Name}>
+                  <Image
+                    src={item.Image}
+                    sx={{ overflow: "hidden",  height: "100%" }}
+                    duration={300}
+                    easing="cubic-bezier(0.7, 0, 0.6, 1)"
+                    showLoading={
+                      <Skeleton
+                        variant="rectangular"
+                        width="100%"
+                        height="200px"
+                        animation="wave"
+                      />
+                    }
+                    errorIcon={true}
+                    shift={null}
+                    distanc="100px"
+                    shiftDuration={900}
+                  />
+                </Grid>
+              );
+            })}
+          </Carousel>
         </Container>
       </Box>
+
       <Container sx={{ py: 8 }} maxWidth="lg">
         {/* End hero unit */}
         <Grid container spacing={4}>
           {cards.map((card) => (
-            <Grid item key={card} xs={12} sm={6} md={4} lg={3} xl={3}>
-              <Card
-                sx={{
-                  width: 320,
-                  maxWidth: "100%",
-                  boxShadow:
-                    "0 2px 4px -2px rgba(0,0,0,0.24), 0 4px 24px -2px rgba(0, 0, 0, 0.2)",
-                }}
-              >
-                <CardActionArea
-                  to={{ pathname: "/watch", search: `?v=${card}` }}
-                  component={Link}
-                >
-                  <CardMedia
-                    image={
-                      "https://image.freepik.com/free-photo/river-foggy-mountains-landscape_1204-511.jpg"
-                    }
-                    sx={{
-                      width: "100%",
-                      height: 0,
-                      paddingBottom: "min(56.25%, 200px)",
-                      bgcolor: "rgba(0, 0, 0, 0.08)",
-                    }}
-                  />
-                </CardActionArea>
-                <CardContent
-                  display="grid"
-                  gridTemplateColumns="repeat(12, 1fr)"
-                  component={Grid}
-                >
-                  <Box gridColumn="span 4">
-                    <Avatar
-                      alt="Remy Sharp"
-                      src="https://i.pravatar.cc/300?img=1"
-                    />
-                  </Box>
-                  <Box gridColumn="span 8">Title</Box>
-                  <Box gridColumn="span 8">Remy Sharp</Box>
-                  <Box gridColumn="span 8">
-                    {new Date().toLocaleDateString()}
-                  </Box>
-                </CardContent>
-              </Card>
-            </Grid>
+            <VideoCard
+              key={card}
+              poster={card}
+              path={card}
+              avatar={card}
+              title={card}
+              name={card}
+            />
           ))}
         </Grid>
       </Container>
