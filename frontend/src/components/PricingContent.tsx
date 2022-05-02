c2a2e038a67a79a5f4afe51cf3be5e73a8baf4c8
supercutcat <supercutcat@outlook.com>
2022-05-02 12:17:26 +0800

    Change price UI

--- frontend/src/components/PricingContent.tsx
+++ frontend/src/components/PricingContent.tsx
@@ -1,89 +1,92 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import StarIcon from '@mui/icons-material/StarBorder';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import GlobalStyles from '@mui/material/GlobalStyles';
-import Container from '@mui/material/Container';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import StarIcon from "@mui/icons-material/StarBorder";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import Container from "@mui/material/Container";
 
 const tiers = [
   {
-    title: 'Free',
-    price: '0',
+    title: "个人会员",
+    price: "30",
     description: [
-      '10 users included',
-      '2 GB of storage',
-      'Help center access',
-      'Email support',
+      "订阅后，即可畅享无广告干扰的观看体验、下载功能及后台播放功能。",
     ],
-    buttonText: 'Sign up for free',
-    buttonVariant: 'outlined',
+    buttonText: "了解详情",
+    buttonVariant: "outlined",
   },
   {
-    title: 'Pro',
-    subheader: 'Most popular',
-    price: '15',
+    title: "家庭会员",
+    subheader: "Most popular",
+    price: "50",
     description: [
-      '20 users included',
-      '10 GB of storage',
-      'Help center access',
-      'Priority email support',
+      "一次订阅可让您和同一个家庭帐号中的 5 名家庭成员（年满 13 周岁）使用所有会员功能。",
     ],
-    buttonText: 'Get started',
-    buttonVariant: 'contained',
+    buttonText: "了解详情",
+    buttonVariant: "contained",
   },
   {
-    title: 'Enterprise',
-    price: '30',
+    title: "学生会员",
+    price: "10",
     description: [
-      '50 users included',
-      '30 GB of storage',
-      'Help center access',
-      'Phone & email support',
+      "您只需验证学生身份，即可以折扣价畅享无广告干扰的视频和音乐以及会员原创内容等。",
     ],
-    buttonText: 'Contact us',
-    buttonVariant: 'outlined',
+    buttonText: "了解详情",
+    buttonVariant: "outlined",
   },
 ];
 const footers = [
   {
-    title: 'Company',
-    description: ['Team', 'History', 'Contact us', 'Locations'],
+    title: "Company",
+    description: ["Team", "History", "Contact us", "Locations"],
   },
   {
-    title: 'Features',
+    title: "Features",
     description: [
-      'Cool stuff',
-      'Random feature',
-      'Team feature',
-      'Developer stuff',
-      'Another one',
+      "Cool stuff",
+      "Random feature",
+      "Team feature",
+      "Developer stuff",
+      "Another one",
     ],
   },
   {
-    title: 'Resources',
-    description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
+    title: "Resources",
+    description: [
+      "Resource",
+      "Resource name",
+      "Another resource",
+      "Final resource",
+    ],
   },
   {
-    title: 'Legal',
-    description: ['Privacy policy', 'Terms of use'],
+    title: "Legal",
+    description: ["Privacy policy", "Terms of use"],
   },
 ];
 
 function PricingContent() {
   return (
     <React.Fragment>
-      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+      <GlobalStyles
+        styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
+      />
       <CssBaseline />
       {/* Hero unit */}
-      <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
+      <Container
+        disableGutters
+        maxWidth="sm"
+        component="main"
+        sx={{ pt: 8, pb: 6 }}
+      >
         <Typography
           component="h1"
           variant="h2"
@@ -91,12 +94,15 @@ function PricingContent() {
           color="text.primary"
           gutterBottom
         >
-          Pricing
+          为您带来无广告干扰的畅听体验
         </Typography>
-        <Typography variant="h5" align="center" color="text.secondary" component="p">
-          Quickly build an effective pricing table for your potential customers with
-          this layout. It&apos;s built with default MUI components with little
-          customization.
+        <Typography
+          variant="h5"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          订阅会员后，即可畅享无广告干扰的观看体验、下载功能及后台播放功能。
         </Typography>
       </Container>
       {/* End hero unit */}
@@ -108,21 +114,21 @@ function PricingContent() {
               item
               key={tier.title}
               xs={12}
-              sm={tier.title === 'Enterprise' ? 12 : 6}
+              sm={tier.title === "Enterprise" ? 12 : 6}
               md={4}
             >
               <Card>
                 <CardHeader
                   title={tier.title}
                   subheader={tier.subheader}
-                  titleTypographyProps={{ align: 'center' }}
-                  action={tier.title === 'Pro' ? <StarIcon /> : null}
+                  titleTypographyProps={{ align: "center" }}
+                  action={tier.title === "Pro" ? <StarIcon /> : null}
                   subheaderTypographyProps={{
-                    align: 'center',
+                    align: "center",
                   }}
                   sx={{
                     backgroundColor: (theme) =>
-                      theme.palette.mode === 'light'
+                      theme.palette.mode === "light"
                         ? theme.palette.grey[200]
                         : theme.palette.grey[700],
                   }}
@@ -130,13 +136,17 @@ function PricingContent() {
                 <CardContent>
                   <Box
                     sx={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'baseline',
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "baseline",
                       mb: 2,
                     }}
                   >
-                    <Typography component="h2" variant="h3" color="text.primary">
+                    <Typography
+                      component="h2"
+                      variant="h3"
+                      color="text.primary"
+                    >
                       ${tier.price}
                     </Typography>
                     <Typography variant="h6" color="text.secondary">
@@ -159,7 +169,7 @@ function PricingContent() {
                 <CardActions>
                   <Button
                     fullWidth
-                    variant={tier.buttonVariant as 'outlined' | 'contained'}
+                    variant={tier.buttonVariant as "outlined" | "contained"}
                   >
                     {tier.buttonText}
                   </Button>
@@ -170,7 +180,7 @@ function PricingContent() {
         </Grid>
       </Container>
       {/* Footer */}
-      <Container
+      {/* <Container
         maxWidth="md"
         component="footer"
         sx={{
@@ -198,7 +208,7 @@ function PricingContent() {
           ))}
         </Grid>
       </Container>
-      {/* End footer */}
+      End footer */}
     </React.Fragment>
   );
 }
